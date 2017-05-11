8ed16cee2b51d9a53e664a783969578efad50afd
eli goldstein <elisings356@gmail.com>
2017-05-11 18:15:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(array)
+to_print = []
+i = 0
+while i < array.length
+  yield(array[i])
+  to_print << "#{array[i]}"
+  i +=1
+end
+to_print
 end
